14ca1e991ac6fa8463c1ab4f4241b514074c254f
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-08-14 18:33:22 -0400

    Start CCC_OO_S4

---
+++ DMOJ/CCC_00_S4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+// https://dmoj.ca/problem/ccc00s4
+
+using namespace std;
+
+typedef vector<int> vi;
+
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define scan(x) cin >> x
+#define scann(x) int x; cin >> x
+#define fast ios::sync_with_stdio(0); cin.tie(0);
+#define local freopen("zDAT.txt", "r", stdin);
+
+int MN = 5280;
+
+
+int main() {
+    fast
+    scann(M);
+
+
+    return 0;
+}
