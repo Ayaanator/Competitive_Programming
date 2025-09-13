fcd803003b4174ee04e02c81e1978bcd22880dd5
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-09-13 11:55:58 -0400

    Solve C

--- Codeforces/Round_1050_Div4/C.cpp
+++ Codeforces/Round_1050_Div4/C.cpp
@@ -21,11 +21,59 @@ void solve() {
 }
 
 int main() {
-    fast
+    local
 
     scann(t);
     while (t--) {
-        
+        int n, m;
+        cin >> n >> m;
+        int s = 0;
+        int p = 0;
+
+        vector<pair<int, int>> tasks;
+
+        FR(i, n) {
+            int a, b;
+            cin >> a >> b;
+            tasks.push_back( { a, b });
+        }
+
+        if (tasks[0].second != s) {
+            if (tasks[0].first < 2) {
+                p++;
+                s = tasks[0].second;
+            }
+        }
+
+        for(int i = 1; i < tasks.size(); i++) {
+            
+            if(s != tasks[i].second) {
+                int diff = tasks[i].first - tasks[i - 1].first;
+
+                if(diff % 2 == 0) {
+                    p += diff - 1;
+                } else {
+                    p += diff;
+                }
+
+            } else {
+                int diff = tasks[i].first - tasks[i - 1].first;
+
+                if(diff % 2 == 0) {
+                    p += diff;
+                } else {
+                    p += diff - 1;
+                }
+            }
+
+            s = tasks[i].second;
+        }
+
+        if(tasks.back().first < m) {
+            p += m - tasks.back().first;
+        }
+
+        cout << p << endl;
     }
 
     return 0;
