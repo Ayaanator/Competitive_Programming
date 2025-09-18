3310be97578f370b4e89b4f49a672b9801902918
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-09-17 22:30:44 -0400

    More fixing of diverse arrays

--- DMOJ/diverse_arrays.cpp
+++ DMOJ/diverse_arrays.cpp
@@ -16,6 +16,7 @@ typedef long long ll;
 #define scanArr(arr, n)  FR(i, n) scan(arr[i])
 #define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
 #define SZ(v) ((int) (v).size())
+
 const int MN = 1e6 + 1; 
 ll N, K;
 int arr[MN], freq[MN];
@@ -28,32 +29,40 @@ int main() {
     fast
 
     int res = 0;
+    int ctr = 0;
+
+    cin >> N >> K;
 
     FR(i, N) scan(arr[i]);
     
-    int l = 0, r=0;
-    
+    int l = 0, r = 0;
+    for(int l = 0 , r = 0; r<N; r++){
 
-    for(int l=0 , r=0; r<N; r++){
-        //process new items
-        //check condition while(condition is true){
+        if(freq[arr[r]] == 0) {
+            freq[arr[r]]++;
+            res++;
 
-            //process our item
-            //l++
-        
-    //}
+            while(res <= K && l < r) {
+                ctr += N - r;
+                l++;
+                freq[arr[l - 1]]--;
+                freq[arr[l]]++;
 
-    //add to the counter or something after 
+                if (freq[arr[l]] == 1) {
+                    res++;
+                }
 
-        if(freq[r] == 0) {
-            
+                if (arr[arr[l - 1]] == 0) {
+                    res--;
+                }
+            }
+
+        } else {
+            freq[r]++;
         }
-
     }
 
-
-    //1 1 1 1 2 1
-    cout << res << endl;
+    cout << ctr << endl;
 
     return 0;
 }
