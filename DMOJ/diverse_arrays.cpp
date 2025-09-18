b235c4e38967aeead7717a10936296250b3b8e7d
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-09-18 12:10:42 -0400

    Solve diverse arrays

--- DMOJ/diverse_arrays.cpp
+++ DMOJ/diverse_arrays.cpp
@@ -16,16 +16,14 @@ typedef long long ll;
 #define scanArr(arr, n)  FR(i, n) scan(arr[i])
 #define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
 #define SZ(v) ((int) (v).size())
+#define int ll
 
 const int MN = 1e6 + 1; 
 ll N, K;
 int arr[MN], freq[MN];
 
-void solve() {
+int32_t main() {
     
-}
-
-int main() {
     fast
 
     int res = 0;
@@ -34,31 +32,26 @@ int main() {
     cin >> N >> K;
 
     FR(i, N) scan(arr[i]);
-    
-    int l = 0, r = 0;
-    for(int l = 0 , r = 0; r<N; r++){
+
+    for(int l = 0 , r = 0; r < N; r++){
 
         if(freq[arr[r]] == 0) {
             freq[arr[r]]++;
             res++;
-
-            while(res <= K && l < r) {
+            //1 1 1
+            while(res >= K) {
                 ctr += N - r;
-                l++;
-                freq[arr[l - 1]]--;
-                freq[arr[l]]++;
+                freq[arr[l]]--;
 
-                if (freq[arr[l]] == 1) {
-                    res++;
-                }
-
-                if (arr[arr[l - 1]] == 0) {
+                if (freq[arr[l]] == 0) {
                     res--;
                 }
+
+                l++;
             }
 
         } else {
-            freq[r]++;
+            freq[arr[r]]++;
         }
     }
 
