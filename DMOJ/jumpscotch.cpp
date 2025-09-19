860fb41036220f061e886f5a3dfaf6cfa21c6b9c
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-09-18 20:25:29 -0400

    Initialize jumpscotch

--- DMOJ/jumpscotch.cpp
+++ DMOJ/jumpscotch.cpp
@@ -17,6 +17,12 @@ typedef long long ll;
 #define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
 #define SZ(v) ((int) (v).size())
 
+const int MX = 1e6 + 1;
+int arr[MX];
+int dp[MX];
+int res[MX];
+int N = 0;
+
 void solve() {
     
 }
@@ -26,6 +32,21 @@ int main() {
 
     scann(N);
     scann(D);
+
+    scanArr(arr, N);
+    int score = arr[0];
+
+    deque<int> q;
+
+    for(int i = 1; i < N; i++) {
+        while(!q.empty() && arr[q.back()] >= arr[i]) q.pop_back();
+        
+        q.push_back(i);
+
+        if(q.front() == i - D) q.pop_front();
+        if(i >= D - 1) res.push_back(nums[q.front()]);
+        
+    }
 
     return 0;
 }
