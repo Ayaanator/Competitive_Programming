752e75488cd94bee9cca24e6e8c6fd993fa1e224
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-10-05 16:26:42 -0400

    Get started on pizza bag

---
+++ DMOJ/DMPG'16_S5.cpp
@@ -0,0 +1,56 @@
+// https://dmoj.ca/problem/dmpg16s5
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
+#define scan(x) cin >> x
+#define scann(x) int x; cin >> x
+#define fast ios::sync_with_stdio(0); cin.tie(0);
+#define local freopen("zDAT.txt", "r", stdin);
+#define scanArr(arr, n)  FR(i, n) scan(arr[i])
+#define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
+#define SZ(v) ((int) (v).size())
+
+const int MX = 200000;
+int arr[MX];
+int prefix[MX];
+
+int main() {
+    local
+
+    scann(N);
+    scann(K);
+
+    FR(i, N) {
+        scann(x);
+        arr[i] = x;
+        arr[i + N] = x;
+    }
+
+    prefix[0] = arr[0];
+    
+    for(int i = 1; i < N * 2; i++) {
+        prefix[i] = prefix[N - 1] + arr[i];
+    }
+
+    deque<int> mn;
+    int mx = -1;
+
+    FR(i, N * 2) {
+        while(!mn.empty() && prefix[mn.back()] > prefix[i]) mn.pop_back();        
+        mn.push_back(i);
+        if(mn.front() == i - K) mn.pop_front();
+
+        mx = max(mx, prefix[i] - prefix[mn.front()]);
+    }
+
+    cout << mx << endl;
+
+    return 0;
+}
