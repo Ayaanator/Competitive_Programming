4b49d53f1ddfc0476e84908694a13b2594d659bd
Ayaan Adrito <ayaan.adrito@gmail.com>
2025-10-15 23:26:26 -0400

    Complete GeeksForGeeks 0-1 knapsack

--- GeeksForGeeks/0-1_knapsack.cpp
+++ GeeksForGeeks/0-1_knapsack.cpp
@@ -19,7 +19,17 @@ typedef long long ll;
 #define dbgArr(arr,n) if(true){ cerr<<#arr<<": "; FR(_i,n) cerr<<(arr)[_i]<<" "; cerr<<endl; }
 
 int knapsack(int W, vector<int> &val, vector<int> &wt) {
-    int dp[wt.size()][val.size()] = {0};
+    int n = val.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= W; j++) {
+            if(wt[i-1] <= j) dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]], dp[i-1][j]);
+            else dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    return dp[n][W];
 }
 
 int main() {
