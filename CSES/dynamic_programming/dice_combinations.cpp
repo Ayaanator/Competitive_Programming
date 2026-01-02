6c36f0df1f7facc76ab86cf368455a32084dcfc0
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-01-02 16:35:21 -0500

    solve .\dice_combinations.cpp

---
+++ CSES/dynamic_programming/dice_combinations.cpp
@@ -0,0 +1,40 @@
+// https://dmoj.ca/problem/cco13p3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
+#define scan(x) cin >> x
+#define scann(x) int x; cin >> x
+#define fast ios::sync_with_stdio(0); cin.tie(0);
+#define local freopen("zDAT.txt", "r", stdin);
+#define scanArr(arr, n)  FR(i, n) scan(arr[i])
+#define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
+#define SZ(v) ((int) (v).size())
+
+const int MX = 1e6 + 1;
+int dp[MX];
+const ll MOD = 1e9 + 7;
+
+int main() {
+  fast
+
+  ll n;
+  cin >> n;
+
+  dp[0] = 1;
+
+  for(int i = 1; i <= n; i++) {
+    for (int j = 1; j <= 6 && j <= i; j++) {
+      dp[i] = (dp[i] + (dp[i - j])) % MOD;
+    }
+  }
+
+  cout << dp[n] << "\n";
+  
+  return 0;
+}
---
+++ CSES/dynamic_programming/minimizing_coins.cpp
@@ -0,0 +1,64 @@
+// https://dmoj.ca/problem/cco13p3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
+#define scan(x) cin >> x
+#define scann(x) int x; cin >> x
+#define fast ios::sync_with_stdio(0); cin.tie(0);
+#define local freopen("zDAT.txt", "r", stdin);
+#define scanArr(arr, n)  FR(i, n) scan(arr[i])
+#define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
+#define SZ(v) ((int) (v).size())
+
+const int MX = 1e6 + 1;
+vector<int> dp(MX, -1);
+set<int,  greater<int>> coins;
+
+int main() {
+  fast
+
+  int n;
+  int x;
+  cin >> n >> x;
+
+  FR(i, n) {
+    scann(j);
+    coins.insert(j);
+    dp[j] = 1;
+  }
+
+  for(int i = 1; i <= x; i++) {
+    int targ = 0;
+    int test = dp[i];
+
+    if(dp[i] == -1) {
+      targ = i;
+      int ctr = 0;
+      bool flag = false;
+
+      for(auto c : coins) {
+        if(c <= targ) {
+          if(targ % c == 0) {
+            int another_test = targ / c;
+            dp[i] = (targ / c) + ctr;
+          } else {
+            int times = i / c;
+            targ = i - (times * c);
+            ctr += times + dp[targ];
+            dp[i] = ctr;
+          }
+        }
+      }
+    }
+  }
+
+  cout << dp[x] << "\n";
+  
+  return 0;
+}
