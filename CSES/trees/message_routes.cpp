df3186271c5b136a2e15dd2142d43387d1ecb3ea
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-01-27 22:19:09 -0500

    start message routes

---
+++ CSES/trees/message_routes.cpp
@@ -0,0 +1,29 @@
+// https://cses.fi/problemset/task/1674
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORR(i, a, b) for (int i = (a); i >= (b); --i)
+#define scan(x) cin >> x
+#define scann(x) int x; cin >> x
+#define fast ios::sync_with_stdio(0); cin.tie(0);
+#define local freopen("zDAT.txt", "r", stdin);
+#define scanArr(arr, n)  FR(i, n) scan(arr[i])
+#define scanArr2D(arr, n, m) FR(i, n) FR(j, m) scan(arr[i][j])
+#define SZ(v) ((int) (v).size())
+
+int main() {
+  fast
+  
+  scann(n);
+
+  for(int i = 1; i < n; i++) {
+    scann(x);
+  }
+   
+  return 0;
+}
