78ee7b04d7d71b69daea2ea164b5b8070214a706
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-01-31 23:02:11 -0500

    work on restaurant customers

---
+++ CSES/sorting_and_searching/restaurant_customers_alt.cpp
@@ -0,0 +1,44 @@
+// https://cses.fi/problemset/task/1091
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define fast ios::sync_with_stdio(0); cin.tie(0);
+#define local freopen("zDAT.txt", "r", stdin);
+
+int main() {
+  fast
+
+  int n;
+  cin >> n;
+
+  int max_time = -1;
+  vector<int> times(1e9 + 1, 0);
+
+  FR(i, n) {
+    int x, y;
+    cin >> x >> y;
+
+    max_time = max(max_time, x);
+    max_time = max(max_time, y);
+
+    times[x] = 1;
+    times[y] = -1;
+  }
+
+  int mx = -1;
+  int ctr = 0;
+
+  for(int i = 0; i <= max_time; i++) {
+    if(times[i] == 1) ctr++;
+    else if(times[i] == -1) ctr--;
+
+    mx = max(mx, ctr);
+  }
+
+  cout << mx << "\n";
+
+  return 0;
+}
