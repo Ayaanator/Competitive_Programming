665321a95b9191e6d69d8b310ffe7c68de8539c0
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-15 18:53:15 -0500

    fix bugs on round trip

--- CSES/graph_theory/round_trip.cpp
+++ CSES/graph_theory/round_trip.cpp
@@ -1,6 +1,8 @@
 // https://cses.fi/problemset/task/1669
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 typedef long long ll;
@@ -10,57 +12,68 @@ typedef long long ll;
 vector<int> node_list;
 int end_node = -1;
 
-bool dfs(int n, int parent, vector<vector<int>> &adj, vector<bool> &vis) {
-  vis[n] = true;
+bool dfs(int n, int parent, vector<vector<int>>& adj, vector<bool>& vis) {
+    vis[n] = true;
 
-  for(auto c : adj[n]) {
-    if(c == parent) continue;
-    node_list.push_back(c);
+    for (auto c : adj[n]) {
+        if (c == parent) continue;
+        node_list.push_back(c);
 
-    if(vis[c]) { 
-      end_node = c;
-      return true;
+        if (vis[c]) {
+            end_node = c;
+            return true;
+        }
+        if (dfs(c, n, adj, vis) == true) return true;
     }
-    if(dfs(c, n, adj, vis) == true) return true;
-  }
 
-  return false;
+    return false;
 }
 
 int main() {
-  local
-  int n = 0; cin >> n;
-  int m = 0; cin >> m;
+    fast
+    int n = 0; cin >> n;
+    int m = 0; cin >> m;
 
-  vector<vector<int>> adj(n + 1);
-  vector<bool> visited(n + 1, false);
+    vector<vector<int>> adj(n + 1);
+    vector<bool> visited(n + 1, false);
 
-  for(int i = 0; i < m; i++) {
-    int a = 0;
-    int b = 0;
-    cin >> a >> b;
-    adj[a].push_back(b);
-    adj[b].push_back(a);
-  }
-
-  for(int i = 1; i <= n; i++) {
-    node_list.push_back(i);
-    if(dfs(i, -1, adj, visited) == true) {
-      
-      int counter = node_list.size() - 2;
-      cout << end_node << " ";
-
-      while(end_node != node_list[counter]) {
-        cout << node_list[counter] << " ";
-        counter--;
-      }
-
-      cout << end_node << "\n";
-      return 0;
+    for (int i = 0; i < m; i++) {
+        int a = 0;
+        int b = 0;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
-  }
-  
-  cout << "IMPOSSIBLE\n";
 
-  return 0;
+    for (int i = 1; i <= n; i++) {
+        node_list.push_back(i);
+        if (dfs(i, -1, adj, visited) == true) {
+            vector<int> res;
+
+            int counter = node_list.size() - 2;
+            //cout << end_node << " ";
+            res.push_back(end_node);
+
+            while (end_node != node_list[counter]) {
+                //cout << node_list[counter] << " ";
+                res.push_back(node_list[counter]);
+                counter--;
+            }
+
+            // cout << end_node << "\n";
+            res.push_back(end_node);
+
+            cout << res.size() << "\n";
+            for (auto n : res) {
+                cout << n << " ";
+            }
+
+            cout << "\n";
+            return 0;
+        }
+    }
+
+    cout << "IMPOSSIBLE\n";
+
+    return 0;
 }
