f85dd0490593f6c69fc5b81255215a513269fcee
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-02-15 19:31:05 -0500

    solve round trip

--- CSES/graph_theory/round_trip.cpp
+++ CSES/graph_theory/round_trip.cpp
@@ -13,67 +13,67 @@ vector<int> node_list;
 int end_node = -1;
 
 bool dfs(int n, int parent, vector<vector<int>>& adj, vector<bool>& vis) {
-    vis[n] = true;
+  vis[n] = true;
 
-    for (auto c : adj[n]) {
-        if (c == parent) continue;
-        node_list.push_back(c);
+  for (auto c : adj[n]) {
+    if (c == parent) continue;
+    node_list.push_back(c);
 
-        if (vis[c]) {
-            end_node = c;
-            return true;
-        }
-        if (dfs(c, n, adj, vis) == true) return true;
+    if (vis[c]) {
+      end_node = c;
+      return true;
     }
+    if (dfs(c, n, adj, vis) == true) return true;
+  }
 
-    return false;
+  node_list.pop_back();
+  return false;
 }
 
 int main() {
-    fast
+  fast
     int n = 0; cin >> n;
-    int m = 0; cin >> m;
+  int m = 0; cin >> m;
 
-    vector<vector<int>> adj(n + 1);
-    vector<bool> visited(n + 1, false);
+  vector<vector<int>> adj(n + 1);
+  vector<bool> visited(n + 1, false);
 
-    for (int i = 0; i < m; i++) {
-        int a = 0;
-        int b = 0;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
+  for (int i = 0; i < m; i++) {
+    int a = 0;
+    int b = 0;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
 
-    for (int i = 1; i <= n; i++) {
-        node_list.push_back(i);
-        if (dfs(i, -1, adj, visited) == true) {
-            vector<int> res;
+  for (int i = 1; i <= n; i++) {
+    node_list.push_back(i);
+    if(!visited[i]) {
+      if (dfs(i, -1, adj, visited) == true) {
+        vector<int> res;
 
-            int counter = node_list.size() - 2;
-            //cout << end_node << " ";
-            res.push_back(end_node);
+        int counter = node_list.size() - 2;
+        res.push_back(end_node);
 
-            while (end_node != node_list[counter]) {
-                //cout << node_list[counter] << " ";
-                res.push_back(node_list[counter]);
-                counter--;
-            }
-
-            // cout << end_node << "\n";
-            res.push_back(end_node);
-
-            cout << res.size() << "\n";
-            for (auto n : res) {
-                cout << n << " ";
-            }
-
-            cout << "\n";
-            return 0;
+        while (end_node != node_list[counter]) {
+          res.push_back(node_list[counter]);
+          counter--;
         }
+
+        res.push_back(end_node);
+
+        cout << res.size() << "\n";
+        for (auto n : res) {
+          cout << n << " ";
+        }
+
+        cout << "\n";
+        return 0;
+      }
     }
+  }
 
-    cout << "IMPOSSIBLE\n";
+  cout << "IMPOSSIBLE\n";
 
-    return 0;
+  return 0;
 }
